7ed97980d3f7a859667faee099455cf2a50c7b28
Grigore <gavrilencugrigore1998@gmail.com>
2023-09-15 09:21:12 +0300

    v222

--- search.js
+++ search.js
@@ -1,16 +1,12 @@
 let result = document.getElementById("searchResults");
 function searchPage() {
-  // Obțineți valoarea introdusă în inputul de căutare
   var searchTerm = document.getElementById("searchInput").value;
 
-  // Obțineți conținutul paginii în care doriți să căutați (puteți folosi document.body pentru întreaga pagină)
   var pageContent = document.body.textContent || document.body.innerText;
 
-  // Folosiți o expresie regulată pentru a căuta termenul pe pagină
   var regex = new RegExp(searchTerm, "gi");
   var matches = pageContent.match(regex);
 
-  // Afișați rezultatele
   var searchResults = document.getElementById("searchResults");
   searchResults.innerHTML = "";
 
@@ -20,7 +16,6 @@ function searchPage() {
       var matchElement = document.createElement("p");
       matchElement.textContent = match;
       searchResults.appendChild(matchElement);
-    
     }
   } else {
     searchResults.textContent = "Niciun rezultat găsit.";
